5ea98a969865813cabcbf5fed80a5f99ec999344
Sean Takafuji <setakafu@microsoft.com>
2021-11-17 03:20:14 +0000

    migrate mixed precision training class

--- contrib/segmentation/src/models/deeplabv3.py
+++ contrib/segmentation/src/models/deeplabv3.py
@@ -1,17 +1,27 @@
+import torch
+import torch.nn as nn
 import torchvision
 from torchvision.models.segmentation.deeplabv3 import DeepLabHead
 
 
-def get_deeplabv3(n_classes: int, pretrained: bool = False, is_feature_extracting: bool = False):
-    model = torchvision.models.segmentation.deeplabv3_resnet50(
-        pretrained=pretrained, aux_loss=True
-    )
+class DeepLabV3(nn.Module):
+    """Wrapper class around torchvision DeepLabV3 to ensure it works with
+    Mixed Precision Training
+    """
 
-    if is_feature_extracting:
-        for param in model.parameters():
-            param.requires_grad = False
+    def __init__(self, n_classes: int, pretrained: bool, is_feature_extracting: bool):
+        super().__init__()
+        self.model = torchvision.models.segmentation.deeplabv3_resnet50(
+            pretrained=pretrained, aux_loss=True
+        )
 
-    model.classifier = DeepLabHead(2048, n_classes)
-    model.aux_classifier = DeepLabHead(1024, n_classes)
+        if is_feature_extracting:
+            for param in self.model.parameters():
+                param.requires_grad = False
 
-    return model
+        self.model.classifier = DeepLabHead(2048, n_classes)
+        self.model.aux_classifier = DeepLabHead(1024, n_classes)
+
+    def forward(self, x):
+        with torch.cuda.amp.autocast():
+            return self.model.forward(x)
