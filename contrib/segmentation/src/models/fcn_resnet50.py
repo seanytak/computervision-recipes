2a99b60f596f726938d43028ae4c6935a3e9494f
Sean Takafuji <setakafu@microsoft.com>
2021-11-17 03:22:01 +0000

    add mixed precision training for fcn resnet50

--- contrib/segmentation/src/models/fcn_resnet50.py
+++ contrib/segmentation/src/models/fcn_resnet50.py
@@ -1,27 +1,33 @@
+import torch
 import torchvision
 from torchvision.models.segmentation.fcn import FCNHead
 
 
-def get_fcn_resnet50(n_classes, pretrained=True, is_feature_extracting: bool = False):
-    """Load Fully Convolutional Network with ResNet-50 backbone
+class FCNResnNet50(nn.Module):
+    def __init__(self, n_classes, pretrained=True, is_feature_extracting: bool = False):
+        """Load Fully Convolutional Network with ResNet-50 backbone
 
-    Parameters
-    ----------
-    n_classes : int
-        Number of classes
-    pretrained : bool
-        True if model should use pre-trained weights from COCO
-    is_feature_extracting : bool
-        True if the convolutional layers should be set to non-trainable retaining their original
-        parameters
-    """
-    model = torchvision.models.segmentation.fcn_resnet50(pretrained=pretrained)
+        Parameters
+        ----------
+        n_classes : int
+            Number of classes
+        pretrained : bool
+            True if model should use pre-trained weights from COCO
+        is_feature_extracting : bool
+            True if the convolutional layers should be set to non-trainable retaining their original
+            parameters
+        """
+        self.model = torchvision.self.models.segmentation.fcn_resnet50(
+            pretrained=pretrained
+        )
 
-    if is_feature_extracting:
-        for param in model.parameters():
-            param.requires_grad = False
+        if is_feature_extracting:
+            for param in self.model.parameters():
+                param.requires_grad = False
 
-    model.classifier = FCNHead(2048, n_classes)
-    model.aux_classifier = FCNHead(1024, n_classes)
+        self.model.classifier = FCNHead(2048, n_classes)
+        self.model.aux_classifier = FCNHead(1024, n_classes)
 
-    return model
+    def forward(self, x):
+        with torch.cuda.amp.autocast():
+            return self.model.forward(x)
